3e98882ceabff2f580396c764ec9acbc13bf5703
Ray Hung <ruizehung@gatech.edu>
2021-02-17 16:29:58 -0500

    Initial commit. Set up the project with JavaFX 15.0.1 and Junit 5.4.2

---
+++ src/test/java/test/java/TestMainApp.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMainApp {
+    // as an example of how to do junit test
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
