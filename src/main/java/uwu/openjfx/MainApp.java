f82db39b992ca376c1e6e2f6dc1114471250609d
James Johnson <choiboyjames@gmail.com>
2021-02-21 23:14:16 -0500

    Add initial game screen

--- src/main/java/uwu/openjfx/MainApp.java
+++ src/main/java/uwu/openjfx/MainApp.java
@@ -11,12 +11,11 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("initialGameScreen.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         
-        stage.setTitle("JavaFX and Gradle");
+        stage.setTitle("UwU");
         stage.setScene(scene);
         stage.show();
     }
