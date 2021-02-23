a7423e9f46578bc14b08a342b9682ee8b25d33b4
Ray Hung <ruizehung@gatech.edu>
2021-02-22 20:52:45 -0500

    add font to labels in initial config screen

--- src/main/java/uwu/openjfx/InitialConfigScreenController.java
+++ src/main/java/uwu/openjfx/InitialConfigScreenController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/main/java/uwu/openjfx/MainApp.java
+++ src/main/java/uwu/openjfx/MainApp.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
