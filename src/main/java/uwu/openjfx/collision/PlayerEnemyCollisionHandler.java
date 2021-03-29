42d6f115e9cf1ac5f27ff7ae49462dcf2ac53ec5
ijagonng <jgonng@outlook.com>
2021-03-29 00:26:24 -0400

    refactor PlayerEnemyCollisionHandler and revise functions for equilibrium

--- src/main/java/uwu/openjfx/collision/PlayerEnemyCollisionHandler.java
+++ src/main/java/uwu/openjfx/collision/PlayerEnemyCollisionHandler.java
@@ -1,10 +1,8 @@
 package uwu.openjfx.collision;
 
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.PhysicsComponent;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import uwu.openjfx.MainApp;
 import uwu.openjfx.RoyalType;
 import uwu.openjfx.components.BossComponent;
@@ -24,7 +22,10 @@ public class PlayerEnemyCollisionHandler extends CollisionHandler {
     private double velocityNormalizeVal = 1.1;
     private boolean equilibriumX = false;
     private boolean equilibriumY = false;
+    private PlayerComponent playerComponent;
+    private PhysicsComponent playerPhysics;
     private EnemyComponent enemyComponent;
+    private PhysicsComponent enemyPhysics;
 
     public PlayerEnemyCollisionHandler() {
         super(RoyalType.PLAYER, RoyalType.ENEMY);
@@ -36,8 +37,11 @@ public class PlayerEnemyCollisionHandler extends CollisionHandler {
         } else {
             enemyComponent = enemy.getComponent(BossComponent.class);
         }
-        // when colliding with an enemy for the first time, evaluate whether or not
-        // player has left and returned, or has already pushed this Mass affected enemy prior.
+
+        /*
+            When colliding with an enemy for the first time, evaluate whether or not
+            player has left and returned, or has already pushed this Mass affected enemy prior.
+         */
         if ((enemyComponent.getPlayerLeavesRadius())) {
             velocityNormalizeVal = 2; // reset velocityNormalizeVal
             equilibriumX = false;
@@ -46,7 +50,7 @@ public class PlayerEnemyCollisionHandler extends CollisionHandler {
     }
 
     public void onCollision(Entity player, Entity enemy) {
-        PlayerComponent playerComponent = player.getComponent(PlayerComponent.class);
+        playerComponent = player.getComponent(PlayerComponent.class);
         HealthComponent playerHealth = playerComponent;
         if (!playerHealth.getIsInvulnerable()) {
             playerHealth.deductHealth();
@@ -54,64 +58,71 @@ public class PlayerEnemyCollisionHandler extends CollisionHandler {
 
         if (!MainApp.isIsTesting()) {
             if (enemyComponent.getMassEffect()) {
-                PhysicsComponent enemyPhysics = enemy.getComponent(PhysicsComponent.class);
-                PhysicsComponent playerPhysics = player.getComponent(PhysicsComponent.class);
+                playerPhysics = player.getComponent(PhysicsComponent.class);
+                enemyPhysics = enemy.getComponent(PhysicsComponent.class);
 
-            /*
-                If enemy is not unstoppable, then start slowly resisting the player's pushing.
-                - If player is pushing right, then push left
-                - If player is pushing left, then push right
-                - If player is pushing down, then push up
-                - If player is pushing up, then push down
-             */
-                if (!equilibriumX) {
-                    enemyPhysics.setVelocityX(enemyPhysics.getVelocityX()
-                            + (playerPhysics.getVelocityX() > 0
-                            ? -velocityNormalizeVal : velocityNormalizeVal));
-                    velocityNormalizeVal *= 1.1;
-                }
-
-                if (!equilibriumY) {
-                    enemyPhysics.setVelocityY(enemyPhysics.getVelocityY()
-                            + (playerPhysics.getVelocityY() > 0
-                            ? -velocityNormalizeVal : velocityNormalizeVal));
-                    velocityNormalizeVal *= 1.1;
-                }
-
-            /*
-                If enemy is pushing at the same magnitude as player, then equilibrium has been
-                achieved. Set enemy's velocity to be permanently equal with player's velocity
-                in the opposite direction. If player is not moving, then player is no longer
-                applying force to the enemy, therefore stop applying equal and opposite force.
-             */
-                if (((Math.abs(enemyPhysics.getVelocityX()) > Math.abs(playerComponent.getSpeed()))
-                        || equilibriumX) && (playerComponent.isPressingMovementKeys())) {
-                    enemyPhysics.setVelocityX(
-                            -playerComponent.getSpeed() * Math.signum(enemyPhysics.getVelocityX()));
-                    enemyComponent.setCollidingWithPlayer(true);
-                    equilibriumX = true;
-                }
-                if (((Math.abs(enemyPhysics.getVelocityY()) > Math.abs(playerComponent.getSpeed()))
-                        || equilibriumY) && (playerComponent.isPressingMovementKeys())) {
-                    enemyPhysics.setVelocityY(
-                            -playerComponent.getSpeed() * Math.signum(enemyPhysics.getVelocityY()));
-                    enemyComponent.setCollidingWithPlayer(true);
-                    equilibriumY = true;
-                }
-
-            /*
-                If player is not moving up/down (only moving left/right), then enemy should
-                also not be moving up/down.
-                If player is not moving left/right (only moving up/down), then enemy should
-                also not be moving left/right.
-             */
-                if (playerPhysics.getVelocityX() == 0) {
-                    enemyPhysics.setVelocityX(0);
-                }
-                if (playerPhysics.getVelocityY() == 0) {
-                    enemyPhysics.setVelocityY(0);
-                }
+                reachEquilibrium();
+                stabilizeEquilibrium();
             }
+        }
+    }
+
+    private void reachEquilibrium() {
+        /*
+            If enemy is not unstoppable, then start slowly resisting the player's pushing.
+            - If player is pushing right, then push left
+            - If player is pushing left, then push right
+            - If player is pushing down, then push up
+            - If player is pushing up, then push down
+         */
+        if (!equilibriumX) {
+            enemyPhysics.setVelocityX(enemyPhysics.getVelocityX()
+                    + (playerPhysics.getVelocityX() > 0
+                    ? -velocityNormalizeVal : velocityNormalizeVal));
+            velocityNormalizeVal *= 1.1;
+        }
+
+        if (!equilibriumY) {
+            enemyPhysics.setVelocityY(enemyPhysics.getVelocityY()
+                    + (playerPhysics.getVelocityY() > 0
+                    ? -velocityNormalizeVal : velocityNormalizeVal));
+            velocityNormalizeVal *= 1.1;
+        }
+
+        /*
+            If player is not moving up/down (only moving left/right), then enemy should
+            also not be moving up/down.
+            If player is not moving left/right (only moving up/down), then enemy should
+            also not be moving left/right.
+         */
+        if (playerPhysics.getVelocityX() == 0) {
+            enemyPhysics.setVelocityX(0);
+        }
+        if (playerPhysics.getVelocityY() == 0) {
+            enemyPhysics.setVelocityY(0);
+        }
+    }
+
+    private void stabilizeEquilibrium() {
+        /*
+            If enemy is pushing at the same magnitude as player, then equilibrium has been
+            achieved. Set enemy's velocity to be permanently equal with player's velocity
+            in the opposite direction. If player is not moving, then player is no longer
+            applying force to the enemy, therefore stop applying equal and opposite force.
+         */
+        if (((Math.abs(enemyPhysics.getVelocityX()) > Math.abs(playerComponent.getSpeed()))
+                || equilibriumX) && (playerComponent.isPressingMovementKeys())) {
+            enemyPhysics.setVelocityX(
+                    -playerComponent.getSpeed() * Math.signum(enemyPhysics.getVelocityX()));
+            enemyComponent.setCollidingWithPlayer(true);
+            equilibriumX = true;
+        }
+        if (((Math.abs(enemyPhysics.getVelocityY()) > Math.abs(playerComponent.getSpeed()))
+                || equilibriumY) && (playerComponent.isPressingMovementKeys())) {
+            enemyPhysics.setVelocityY(
+                    -playerComponent.getSpeed() * Math.signum(enemyPhysics.getVelocityY()));
+            enemyComponent.setCollidingWithPlayer(true);
+            equilibriumY = true;
         }
     }
 }
