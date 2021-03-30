c53c49231da9fd18c59de8fa9203a5805b9f81cf
James Johnson <jjohnson660@gatech.edu>
2021-03-29 20:58:59 -0400

    Tweak sword ult animation

--- src/main/java/uwu/openjfx/weapons/GoldenSword0.java
+++ src/main/java/uwu/openjfx/weapons/GoldenSword0.java
@@ -30,7 +30,7 @@ public class GoldenSword0 implements Weapon {
                         put("duration", getDuration(ultimateActivated)).
                         put("frameWidth", width).
                         put("frameHeight", height).
-                        put("fpr", !ultimateActivated ? 6 : 6));
+                        put("fpr", !ultimateActivated ? 6 : 5));
         // Spawn the sword at player's "hands"
         gs.getTransformComponent().setAnchoredPosition(
                 new Point2D(player.getX() - ((double) width / 2) + player.getWidth() / 2,
