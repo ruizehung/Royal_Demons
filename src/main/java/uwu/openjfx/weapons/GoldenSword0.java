dd302d04082621b34a5ed5cd9b1dcf2f32613f4a
ijagonng <jgonng@outlook.com>
2021-04-08 14:17:02 -0400

    fix sword still staying longer than it should bug

--- src/main/java/uwu/openjfx/weapons/GoldenSword0.java
+++ src/main/java/uwu/openjfx/weapons/GoldenSword0.java
@@ -64,12 +64,6 @@ public class GoldenSword0 implements Weapon {
                         player.getX() - ((double) hitBoxWidth / 2) + player.getWidth() / 2
                                 + (player.getScaleX() > 0 ? swordOffset : -swordOffset),
                         player.getY() - ((double) hitBoxHeight / 2) + player.getHeight() / 2));
-        // Delete the hitbox after attacking
-//        FXGL.getGameTimer().runAtInterval(() -> {
-//            if (meleeHitBox.isActive()) {
-//                meleeHitBox.removeFromWorld();
-//            }
-//        }, Duration.seconds(.1));
     }
 
     @Override
@@ -99,6 +93,4 @@ public class GoldenSword0 implements Weapon {
     public String getDescription() {
         return "Some description";
     }
-
-
 }
