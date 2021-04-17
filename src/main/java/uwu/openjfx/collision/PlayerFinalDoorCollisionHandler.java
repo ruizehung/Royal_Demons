ccd78e3386f6f7b78df02f887c763f98e6d2b557
Ray Hung <ruizehung@gatech.edu>
2021-04-17 12:22:29 -0400

    fix final boss door and add final win room

--- src/main/java/uwu/openjfx/collision/PlayerFinalDoorCollisionHandler.java
+++ src/main/java/uwu/openjfx/collision/PlayerFinalDoorCollisionHandler.java
@@ -3,7 +3,13 @@ package uwu.openjfx.collision;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.physics.CollisionHandler;
+import uwu.openjfx.MapGeneration.Coordinate;
+import uwu.openjfx.MapGeneration.GameMap;
+import uwu.openjfx.MapGeneration.Room;
 import uwu.openjfx.RoyalType;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getInput;
 
 public class PlayerFinalDoorCollisionHandler extends CollisionHandler {
 
@@ -14,9 +20,17 @@ public class PlayerFinalDoorCollisionHandler extends CollisionHandler {
     @Override
     protected void onCollisionBegin(Entity player, Entity finalDoor) {
         if (FXGL.getGameWorld().getEntitiesByType(RoyalType.ENEMY).size() > 0) {
-            System.out.println("Teleport Player to final room");
-        } else {
             System.out.println("Player should kill the boss first");
+        } else {
+            System.out.println("Teleport Player to final room");
+            getInput().setProcessInput(false);
+            GameMap gameMap = FXGL.geto("gameMap");
+            Room finalWinRoom = new Room(new Coordinate(999, 999));
+            finalWinRoom.setRoomType("finalWinRoom");
+            getGameScene().getViewport().fade(() -> {
+                gameMap.loadRoom(finalWinRoom, "center");
+                getInput().setProcessInput(true);
+            });
         }
 
     }
