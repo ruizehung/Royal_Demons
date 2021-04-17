a429db2f1eb939ffbae4a54a0823145aa775d11a
Devan Moses <dmoses@gatech.edu>
2021-04-17 12:35:16 -0400

    boss cutscene and endgame

--- src/main/java/uwu/openjfx/collision/PlayerFinalDoorCollisionHandler.java
+++ src/main/java/uwu/openjfx/collision/PlayerFinalDoorCollisionHandler.java
@@ -1,9 +1,14 @@
 package uwu.openjfx.collision;
 
+import com.almasb.fxgl.cutscene.Cutscene;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.physics.CollisionHandler;
 import uwu.openjfx.RoyalType;
+import uwu.openjfx.components.PlayerComponent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlayerFinalDoorCollisionHandler extends CollisionHandler {
 
@@ -13,8 +18,19 @@ public class PlayerFinalDoorCollisionHandler extends CollisionHandler {
 
     @Override
     protected void onCollisionBegin(Entity player, Entity finalDoor) {
-        if (FXGL.getGameWorld().getEntitiesByType(RoyalType.ENEMY).size() > 0) {
+        if (FXGL.getGameWorld().getEntitiesByType(RoyalType.ENEMY).size() <= 0) {
             System.out.println("Teleport Player to final room");
+            ArrayList<String> savePrincess = new ArrayList<>(Arrays.asList(
+                    "1.name = " + PlayerComponent.getPlayerName(),
+                    "2.name = " + PlayerComponent.getPlayerName() + "'s dino lover",
+                    "1.image = ui_yoshi.png",
+                    "2.image = ui_peach.png",
+                    "1: omg is dat u??",
+                    "2: ya boo, thnx for saving me",
+                    "1: np, lets go home and watch some netflix ^^"
+            ));
+            Cutscene cs = new Cutscene(savePrincess);
+            FXGL.getCutsceneService().startCutscene(new Cutscene(savePrincess));
         } else {
             System.out.println("Player should kill the boss first");
         }
--- src/main/java/uwu/openjfx/input/TeleportToBossRoom.java
+++ src/main/java/uwu/openjfx/input/TeleportToBossRoom.java
@@ -1,8 +1,14 @@
 package uwu.openjfx.input;
 
+import com.almasb.fxgl.cutscene.Cutscene;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.input.UserAction;
+import javafx.util.Duration;
 import uwu.openjfx.MapGeneration.GameMap;
+import uwu.openjfx.components.PlayerComponent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
 
@@ -17,5 +23,19 @@ public class TeleportToBossRoom extends UserAction {
         getGameScene().getViewport().fade(() -> {
             gameMap.loadRoom(gameMap.getBossRoom(), "west");
         });
+        ArrayList<String> bossFight = new ArrayList<>(Arrays.asList(
+                "1.name = " + PlayerComponent.getPlayerName(),
+                "2.name = boss guy",
+                "1.image = ui_yoshi.png",
+                "2.image = ui_peach.png",
+                "1: where mah boo?",
+                "2: theyre NOT behind the only door in the room,"
+                        + "to which i ate the key and have been constipated"
+                        + " for some time now MUAHAHAHA",
+                "1: thats gross man..."
+        ));
+        FXGL.getSceneService().getTimer().runOnceAfter(() -> FXGL.getCutsceneService()
+                .startCutscene(
+                        new Cutscene(bossFight)), Duration.millis(1500));
     }
 }
